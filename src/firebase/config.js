1d94280fc9e0a166b509a9e7937cbc3c72ee6067
Vani2904 <142568607+Vani2904@users.noreply.github.com>
2024-08-23 23:46:22 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,14 @@ import { getFirestore, serverTimestamp } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  authDomain: "instagram---clones.firebaseapp.com",
-  projectId: "instagram---clones",
-  storageBucket: "instagram---clones.appspot.com",
-  messagingSenderId: "763195397310",
-  appId: "1:763195397310:web:d3a523b995aa32a131faef"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
